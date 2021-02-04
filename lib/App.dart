7ea187c7a3bef308b34ef583a142bc60ce9051a1
Diego Luna <magin1104@gmail.com>
2021-02-04 08:35:24 -0400

    FirstCommit

---
+++ lib/App.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('HOLA MUNDO'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Views/CondicionVenta.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CondicionVentas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
